602e03e7d9350ea7cd30fdd4a7cf1527894c2c93
ArtemLevin <python_study_2022@mail.ru>
2022-11-30 00:17:14 +0300

    Task 2

---
+++ Hometask_1/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b = ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+
+{
+    Console.Write("a =" + " " + a + ";" + " " + "b =" + " " + b + " -> " + "max = " );
+    Console.WriteLine(a);
+}
+if (a < b)
+
+{
+    Console.Write("a =" + " " + a + ";" + " " + "b =" + " " + b + " -> " + "max = " );
+    Console.WriteLine(b);
+}
+
+if (a == b)
+
+{
+Console.Write("a =" + " " + a + ";" + " " + "b =" + " " + b + " -> " + "Числа равны");   
+}
